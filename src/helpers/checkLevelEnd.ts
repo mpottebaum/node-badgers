e9c2fe5d3ff537028e05ecfe1b13fe6d13ecf9c4
Michael Pottebaum <michael.pottebaum@cdw.com>
2021-10-21 15:26:05 -0500

    converted checkLevelEnd.js to ts

--- src/helpers/checkLevelEnd.ts
+++ src/helpers/checkLevelEnd.ts
@@ -1,4 +1,7 @@
-const checkWin = (user, badgers) => {
+import User from "../models/user"
+import Badgers from "../models/badgers"
+
+const checkWin = (user: User, badgers: Badgers) => {
     if(user.coordinates.y === 0 && [20, 21, 22, 23].includes(user.coordinates.x)) {
         user.win = true
     } else if(badgers.current().length === 0) {
@@ -6,13 +9,13 @@ const checkWin = (user, badgers) => {
     }
 }
 
-const checkAlive = (user, badgers) => {
+const checkAlive = (user: User, badgers: Badgers) => {
     if(badgers.killerBadger()) {
         user.alive = false
     }
 }
 
-const checkLevelEnd = (user, badgers) => {
+const checkLevelEnd = (user: User, badgers: Badgers) => {
     checkWin(user, badgers)
     checkAlive(user, badgers)
     return (!user.alive || user.win)
