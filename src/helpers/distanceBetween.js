2053bbdc5345ecf952a4fb5abfd26063dc9d9751
Michael Pottebaum <michael.pottebaum@cdw.com>
2021-10-21 15:27:22 -0500

    converted distanceBetween.js to ts

--- src/helpers/distanceBetween.js
+++
@@ -1,9 +0,0 @@
-export const yDistanceBetween = (user, badger) => badger.coordinates.y - user.coordinates.y
-
-export const xDistanceBetween = (user, badger) => (badger.coordinates.x - user.coordinates.x) * 2
-
-export const distanceBetween = (user, badger) => {
-    const yLength = Math.abs(yDistanceBetween(user, badger))
-    const xLength = Math.abs(xDistanceBetween(user, badger))
-    return Math.sqrt(yLength**2 + xLength**2)
-}
